9809a0861f436b916c01a83089f0f920f37d18f7
devttys0 <heffnercj@gmail.com>
2017-03-17 23:15:16 -0400

    Added data structure identification to codatify

--- plugins/codatify/codatify.py
+++ plugins/codatify/codatify.py
@@ -8,6 +8,218 @@
 import idc
 import idaapi
 import idautils
+import string
+
+class StructEntry(object):
+
+    def __init__(self, ea):
+        self.ea = ea
+        self.dword = idc.Dword(self.ea)
+        self.type = None
+        self.value = None
+
+        string = idc.GetString(self.dword)
+        name = idc.GetFunctionName(self.dword)
+        if idc.LocByName(name) != self.dword:
+            name = ''
+
+        if name:
+            self.type = int
+            self.value = name
+        elif string:
+            self.type = str
+            self.value = string
+
+class StructCast(object):
+
+    def __init__(self, ea, n=16):
+        self.entries = []
+
+        for i in range(0, n):
+            self.entries.append(StructEntry(ea+(4*i)))
+
+class StructPatternDetector(object):
+
+    def __init__(self, ea, n=16):
+        self.address = ea
+        self.stop = None
+        self.start = None
+        self.element_size = 4
+        self.num_entries = 0
+        self.num_elements = 0
+        self.function_element = -1
+        self.name_element = -1
+        entry_element_count = None
+
+        struct = StructCast(ea, n)
+
+        if struct.entries[0].type is not None:
+            (p1, p2) = self.get_entry_pair(struct)
+
+            pattern_to_match = [x.type for x in struct.entries[p1:p2+1]]
+
+            for i in range(0, n):
+                address = ea + ((p2+i)*self.element_size)
+                struct2 = StructCast(address, n)
+                this_pattern = [x.type for x in struct2.entries[p1:p2+1]]
+                if this_pattern == pattern_to_match:
+                    entry_element_count = (address - ea) / self.element_size
+                    break
+
+            if entry_element_count is not None and entry_element_count > 0:
+                num_entries = self.get_entry_count(ea,
+                                                   entry_element_count,
+                                                   [x.type for x in struct.entries[p1:entry_element_count]])
+
+                self.start = self.address
+                self.num_elements = entry_element_count
+                self.num_entries = num_entries
+                self.stop = self.address + (self.num_elements * self.element_size * self.num_entries)
+
+                if struct.entries[p1].type == str:
+                    self.name_element = p1
+                    self.function_element = p2
+                else:
+                    self.name_element = p2
+                    self.function_element = p1
+
+
+    def get_entry_count(self, start, entry_element_count, expected_pattern):
+        entry_count = 1
+
+        while True:
+            address = start + (entry_element_count * self.element_size * entry_count)
+            struct = StructCast(address, entry_element_count)
+            this_pattern = [x.type for x in struct.entries[0:entry_element_count]]
+            if this_pattern != expected_pattern:
+                break
+            else:
+                entry_count += 1
+
+        return entry_count
+
+
+    def get_entry_pair(self, struct):
+        end = 0
+        start = 0
+        count = 0
+
+        for entry in struct.entries:
+            if entry.type is not None and entry.type != struct.entries[0].type:
+                end = count
+                break
+            count += 1
+
+        return (start, end)
+
+
+class Struct(object):
+
+    def __init__(self, **kwargs):
+        for (k, v) in kwargs.iteritems():
+            setattr(self, k, v)
+
+class StructFinder(object):
+
+    def __init__(self):
+        (self.start, self.stop) = self.get_data_section()
+
+    def get_data_section(self):
+        ea = idc.BADADDR
+        seg = idc.FirstSeg()
+
+        while seg != idc.BADADDR:
+            if ea == idc.BADADDR and idc.GetSegmentAttr(seg, idc.SEGATTR_TYPE) == 2:
+                ea = seg
+
+            stop = idc.SegEnd(seg)
+            seg = idc.NextSeg(seg)
+
+        return (ea, stop)
+
+    def valid_function_name(self, name):
+        allowed_characters = set(string.digits + string.ascii_letters + '_')
+        allowed_first_character = set(string.ascii_letters + '_')
+
+        if not name:
+            return False
+        elif name[0] not in allowed_first_character:
+            return False
+        elif len([x for x in name if x not in allowed_characters]) > 0:
+            return False
+
+        return True
+
+    def search(self):
+        patterns = []
+
+        print "Searching for data structure arrays from", hex(self.start), "to", hex(self.stop)
+
+        ea = self.start
+        while ea < self.stop:
+            pattern = StructPatternDetector(ea)
+            if pattern.num_entries > 0:
+                patterns.append(pattern)
+                ea = pattern.stop
+            else:
+                ea += pattern.element_size
+
+        i = 0
+        while i < len(patterns):
+            # Consolidate obvious duplicate consecutive structure arrays
+            j = i + 1
+            while j < len(patterns):
+                if (patterns[i].stop == patterns[j].start and
+                    patterns[i].num_elements == patterns[j].num_elements and
+                    patterns[i].function_element == patterns[j].function_element and
+                    patterns[i].name_element == patterns[i].name_element):
+                    patterns[i].stop = patterns[j].stop
+                    patterns[i].num_entries += patterns[j].num_entries
+                    del patterns[j]
+                else:
+                    j += 1
+
+            print "Found an array of %d structures at 0x%X - 0x%X. Each entry has %d elements of %d bytes each." % (patterns[i].num_entries,
+                                                                                                                    patterns[i].start,
+                                                                                                                    patterns[i].stop,
+                                                                                                                    patterns[i].num_elements,
+                                                                                                                    patterns[i].element_size)
+            print "Array element #%d is the address pointer, and element #%d is the address pointer name.\n" % (patterns[i].function_element, patterns[i].name_element)
+
+            i += 1
+
+        return patterns
+
+    def parse_function_tables(self):
+        count = 0
+
+        for pattern in self.search():
+            name2func = {}
+
+            ea = pattern.start
+            while ea < pattern.stop:
+                string_address = idc.Dword(ea + (pattern.name_element * pattern.element_size))
+                function_address = idc.Dword(ea + (pattern.function_element * pattern.element_size))
+
+                new_function_name = idc.GetString(string_address)
+                current_function_name = idc.Name(function_address)
+
+                if not self.valid_function_name(new_function_name):
+                    print "ERROR: '%s' is not a valid function name. This is likely not a function table, or I have parsed it incorrectly!" % new_function_name
+                    print "       Ignoring all entries in the structures between 0x%X and 0x%X.\n" % (pattern.start, pattern.stop)
+                    name2func = {}
+                    break
+                elif current_function_name.startswith("sub_"):
+                    name2func[new_function_name] = function_address
+
+                ea += (pattern.num_elements * pattern.element_size)
+
+            for (name, address) in name2func.iteritems():
+                print "0x%.8X => %s" % (address, name)
+                idc.MakeName(address, name)
+                count += 1
+
+        print "Renamed %d functions!" % count
 
 class Codatify(object):
 
@@ -171,6 +383,7 @@ class codatify_t(idaapi.plugin_t):
         cd.stringify()
         cd.datify()
         cd.pointify()
+        StructFinder().parse_function_tables()
 
 def PLUGIN_ENTRY():
     return codatify_t()
